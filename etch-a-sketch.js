50036f59a28bf2aa82d9ba18f7fc0f0e6cadf9cf
ENAwareness <166632095+ENAwareness@users.noreply.github.com.>
2025-01-16 21:20:50 +0900

    Add hover effect using mouseenter event

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,6 +3,10 @@ const container = document.getElementById("container");
 for (let i = 0; i < 16 * 16; i++) {
   const square = document.createElement("div");
   square.classList = "grid-square";
+
+  square.addEventListener("mouseenter", function (e) {
+    e.target.classList.add('hover');
+  });
+
   container.appendChild(square);
 }
-
