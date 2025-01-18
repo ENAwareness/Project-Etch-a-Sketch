4f0e8f42f64d11e0f0c593c29958b8286bf8b0ee
ENAwareness <166632095+ENAwareness@users.noreply.github.com.>
2025-01-18 14:01:11 +0900

    Add button to create a new grid with user-specified dimensions

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,31 @@
 const container = document.getElementById("container");
+const btnChangeGrid = document.getElementById("btn-change-grid");
 
-for (let i = 0; i < 16 * 16; i++) {
-  const square = document.createElement("div");
-  square.classList = "grid-square";
+btnChangeGrid.addEventListener("click", function () {
+  let size = prompt("Input the number of squares per side");
+  if (Number(size) <= 100) {
+    createGrid(Number(size));
+  } else {
+    alert("Please enter a number less than or equal to 100");
+  }
+});
 
-  square.addEventListener("mouseenter", function (e) {
-    e.target.classList.add('hover');
-  });
+function createGrid(size) {
+  container.innerHTML = "";
 
-  container.appendChild(square);
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("grid-square");
+
+    square.style.width = `${960/size}px`;
+    square.style.height = `${960/size}px`;
+
+    square.addEventListener("mouseenter", function (e) {
+      e.target.classList.add("hover");
+    });
+
+    container.appendChild(square);
+  }
 }
+
+createGrid(16);
